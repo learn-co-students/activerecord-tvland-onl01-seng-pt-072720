45de7b2fce45aef1f3cca533635c8be1f8375e91
jmw0426 <jmw0426@hotmail.com>
2020-10-07 16:14:05 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,9 @@
 class Show < ActiveRecord::Base
     has_many :characters
     has_many :actors, through: :characters
+    belongs_to :network
 
-    # def actors_list
-
-    # end
+    def actors_list
+        self.characters.collect { |char| char.actor.first_name + " " + char.actor.last_name}
+    end
 end
