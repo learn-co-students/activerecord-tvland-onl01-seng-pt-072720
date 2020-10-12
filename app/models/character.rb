b8f683cb4990f81396f73632cac6f725f9bad1cb
Shasel <github email address>
2020-10-12 02:28:44 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,17 @@
 class Character < ActiveRecord::Base
+  belongs_to :show
+  belongs_to :actor
+
+  def say_that_thing_you_say
+    catchphrase = "#{self.name} always says: #{self.catchphrase}"
+  end
+
+#   def build_shows(name)
+#     self.shows << name
+#   end
   
+#   def build_network(call_letters)
+#     self.shows.network
+#   end
+
 end
