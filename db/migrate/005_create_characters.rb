c914629f4a371ca2a0bfc12c2137fcce06a1a6b8
github username <github email address>
2020-10-12 15:58:03 -0500

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,9 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+    def change
+        create_table :characters do |x|
+            x.string :name
+            x.integer :actor_id
+            x.integer :show_id
+        end
+    end
+end
