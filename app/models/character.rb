468130fb26a909a6071ecb1a6d63c5a043f8e597
yani82 <yani.iman1@gmail.com>
2020-10-12 22:57:32 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
+
+    belongs_to :actor 
+    belongs_to :show
+
+    # name, actor_id, and a show_id, adds the column catchphrase
+
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end 
   
 end
