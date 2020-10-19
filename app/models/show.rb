6c16115768a5c95591412c729b5a33e3b3864334
Kelin Field <kfield111@hotmail.com>
2020-10-19 01:14:16 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    array = []
+    self.actors.each do |temp|
+      array << "#{temp.first_name} #{temp.last_name}"
+    end
+    array
+  end
+
 end
